51921b092b81cfaf28c9349d333ebac7f02db0db
nikita68 <n.makarov@hotmail.com>
2017-10-11 17:01:13 +0200

    Minor restructuring

---
+++ seq2seq/attn-seq2seq/helpers.py
@@ -0,0 +1,42 @@
+import numpy as np
+import random
+
+
+def batch(x, max_sequence_length=10):
+	"""
+	See https://github.com/ematvey/tensorflow-seq2seq-tutorials/blob/master/helpers.py
+	:param x:
+	:return:
+	"""
+	sequence_lengths = [len(i) for i in x]
+	max_length = max_sequence_length
+	for i in x:
+		while len(i) < max_length:
+			i.append(0)
+	sequence_array = np.asarray(x).T
+	return sequence_array, sequence_lengths
+
+
+def generate_random_lists(amount=10000, min_size=5, max_size=10, min_n=2, max_n=9):
+	"""
+	Generates a specified amount of lists in a list, with each list being random in length and filled with random
+	numbers.
+	:param amount:
+	:param min_size:
+	:param max_size:
+	:param min_n:
+	:param max_n:
+	:return:
+	"""
+	rlist = []
+	for i in range(amount):
+		length = random.randrange(min_size, max_size)
+		tlist = []
+		for l in range(length):
+			tlist.append(random.randrange(min_n, max_n))
+		rlist.append(tlist)
+	return rlist
+
+
+
+	
