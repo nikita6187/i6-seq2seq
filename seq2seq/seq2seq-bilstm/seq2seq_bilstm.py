0cc7ac1ba2b6b2fe7cf8095c25d080f9b0537086
nikita68 <n.makarov@hotmail.com>
2017-10-12 17:43:28 +0200

    Seq2seq blstm

--- seq2seq/seq2seq-bilstm/seq2seq_bilstm.py
+++ seq2seq/seq2seq-bilstm/seq2seq_bilstm.py
@@ -28,9 +28,11 @@ encoder_inputs_embedded = tf.nn.embedding_lookup(embeddings, encoder_inputs)
 encoder_cell = LSTMCell(encoder_hidden_units)
 ((encoder_fw_outputs, encoder_bw_outputs),
  (encoder_fw_final_state, encoder_bw_final_state)) = tf.nn.bidirectional_dynamic_rnn(cell_fw=encoder_cell,
-                                            cell_bw=encoder_cell, inputs=encoder_inputs_embedded,
-                                            time_major=True,sequence_length=encoder_inputs_length,
-                                            dtype=tf.float32)
+                                                                                     cell_bw=encoder_cell,
+                                                                                     inputs=encoder_inputs_embedded,
+                                                                                     time_major=True,
+                                                                                     sequence_length=encoder_inputs_length,
+                                                                                     dtype=tf.float32)
 
 encoder_outputs = tf.concat((encoder_fw_outputs, encoder_bw_outputs), 2)
 encoder_final_state_c = tf.concat((encoder_fw_final_state.c, encoder_bw_final_state.c), 1)
@@ -45,26 +47,51 @@ decoder_lengths = encoder_inputs_length + 3  # +2 additional steps, +1 for EOS
 eos_time_slice = tf.ones([batch_size], tf.int32, name='EOS')
 pad_time_slice = tf.zeros([batch_size], tf.int32, name='PAD')
 
-tf.nn.embedding_lookup(embeddings, eos_time_slice)
-tf.nn.embedding_lookup(embeddings, pad_time_slice)
+eos_step_embedded = tf.nn.embedding_lookup(embeddings, eos_time_slice)
+pad_step_embedded = tf.nn.embedding_lookup(embeddings, pad_time_slice)
+
+W = tf.Variable(tf.random_uniform([decoder_hidden_units, vocab_size], -1, 1), dtype=tf.float32)
+b = tf.Variable(tf.zeros([vocab_size]), dtype=tf.float32)
 
 
 def loop_fn_initial():
-	initial_elements_finished = (0 >= decoder_lengths)
-	initial_input = eos_time_slice
-	initial_cell_state = encoder_final_state
-	initial_cell_output = None
-	initial_loop_state = None
-	return (initial_elements_finished, initial_input, initial_cell_state, initial_cell_output, initial_loop_state)
+    initial_elements_finished = (0 >= decoder_lengths)
+    initial_input = eos_step_embedded
+    initial_cell_state = encoder_final_state
+    initial_cell_output = None
+    initial_loop_state = None
+    return initial_elements_finished, initial_input, initial_cell_state, initial_cell_output, initial_loop_state
 
 
-def loop_fn_tranistion():
-	# TODO!
+def loop_fn_transition(time, previous_output, previous_state, previous_loop_state):
+    def get_next_input():
+        output_logits = tf.add(tf.matmul(previous_output, W), b)
+        prediction = tf.argmax(output_logits, axis=1)
+        next_input = tf.nn.embedding_lookup(embeddings, prediction)
+        return next_input
 
-decoder_outputs, decoder_final_state = tf.nn.dynamic_rnn(decoder_cell, decoder_inputs_embedded,
-                                                         initial_state=encoder_final_state, dtype=tf.float32,
-                                                         time_major=True, scope="plain_decoder")
-decoder_logits = tf.contrib.layers.fully_connected(decoder_outputs, vocab_size, activation_fn=None)
+    elements_finished = (time >= decoder_lengths)  # produces tensor shape [batch_size]; says which elements are fin
+    finished = tf.reduce_all(elements_finished)  # true if all are finished else false
+    next_input = tf.cond(finished, lambda: pad_step_embedded, get_next_input)  # if finished PAD else provide next input
+    state = previous_state
+    output = previous_output
+    loop_state = None
+    return elements_finished, next_input, state, output, loop_state
+
+
+def loop_fn(time, previous_output, previous_state, previous_loop_state):
+    if previous_state is None:  # time = 0
+        return loop_fn_initial()
+    else:
+        return loop_fn_transition(time, previous_output, previous_state, previous_loop_state)
+
+decoder_outputs_ta, decoder_final_state, _ = tf.nn.raw_rnn(decoder_cell, loop_fn)
+decoder_outputs = decoder_outputs_ta.stack()
+
+decoder_max_steps, decoder_batch_size, decoder_dims = tf.unstack(tf.shape(decoder_outputs))
+decoder_outputs_flat = tf.reshape(decoder_outputs, (-1, decoder_dims))
+decoder_logits_flat = tf.add(tf.matmul(decoder_outputs_flat, W), b)
+decoder_logits = tf.reshape(decoder_logits_flat, (decoder_max_steps, decoder_batch_size, vocab_size))
 decoder_prediction = tf.argmax(decoder_logits, 2)
 
 # Training
@@ -78,33 +105,32 @@ init = tf.global_variables_initializer()
 
 
 def next_batch(amount=100):
-	random_lists_raw = helpers.generate_random_lists(amount=amount)
-	e_in, _ = helpers.batch(random_lists_raw)
-	d_targets,_ = helpers.batch([(sequence) + [EOS] for sequence in random_lists_raw])
-	d_in,_ = helpers.batch([[EOS] + (sequence) for sequence in random_lists_raw])
-	return {
-		encoder_inputs: e_in,
-		decoder_targets: d_targets,
-		decoder_inputs: d_in,
-	}
+    random_lists_raw = helpers.generate_random_lists(amount=amount)
+    e_in, e_in_length = helpers.batch(random_lists_raw)
+    d_targets, _ = helpers.batch([(sequence) + [EOS] + [PAD] * 2 for sequence in random_lists_raw])
+    return {
+        encoder_inputs: e_in,
+        encoder_inputs_length: [10]*amount, #e_in_length,
+        decoder_targets: d_targets,
+    }
 
 
 with tf.Session() as sess:
-	sess.run(init)
-	losses = []
-	
-	for batch in range(3000):
-		feed = next_batch()
-		_, l = sess.run([train_op, loss], feed)
-		losses.append(l)
+    sess.run(init)
+    losses = []
 
-		if batch % 100 == 0:
-			print('Batch: {0} Loss:{1:2f}'.format(batch, losses[-1]))
-			predict = sess.run(decoder_prediction, feed)
-			for i, (inp, pred) in enumerate(zip(feed[encoder_inputs].T, predict.T)):
-				print(' Sample {0}'.format(i+1))
-				print('  Input     > {0}'.format(inp))
-				print('  Predicted > {0}'.format(pred))
-				if i > 2:
-					break
-			print
+    for batch in range(3000):
+        feed = next_batch()
+        _, l = sess.run([train_op, loss], feed)
+        losses.append(l)
+
+        if batch % 100 == 0:
+            print('Batch: {0} Loss:{1:2f}'.format(batch, losses[-1]))
+            predict = sess.run(decoder_prediction, feed)
+            for i, (inp, pred) in enumerate(zip(feed[encoder_inputs].T, predict.T)):
+                print(' Sample {0}'.format(i + 1))
+                print('  Input     > {0}'.format(inp))
+                print('  Predicted > {0}'.format(pred))
+                if i > 2:
+                    break
+            print
