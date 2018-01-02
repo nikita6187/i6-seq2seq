c3ddbf3f3f972a8265f1534be66a61e98c92e539
nikita68 <n.makarov@hotmail.com>
2018-01-02 18:23:06 +0100

    Finished initial alignment methods & basic debugging

--- neural-transducer/neural_transducer.py
+++ neural-transducer/neural_transducer.py
@@ -3,12 +3,13 @@ from tensorflow.contrib.rnn import LSTMCell, LSTMStateTuple
 from tensorflow.python.layers import core as layers_core
 import numpy as np
 import helpers
+import copy
 
 # NOTE: Time major
 
 # Constants
 input_dimensions = 1
-vocab_size = 10
+vocab_size = 3
 input_embedding_size = 20
 encoder_hidden_units = 64
 inputs_embedded = True
@@ -16,28 +17,48 @@ transducer_hidden_units = 64
 batch_size = 1
 GO_SYMBOL = vocab_size - 1  # TODO: Make these constants correct
 END_SYMBOL = vocab_size
+input_block_size = 10
+log_prob_init_value = 0
 
 # ---------------- Helper classes -----------------------
 
 
 class Alignment(object):
     def __init__(self):
-        self.alignment_position = (0, 0)  # x = position in target, y = block index
-        self.log_prob = 0  # The sum log prob of this alignment over the target indices
+        self.alignment_position = (1, 1)  # x = position in target (y~), y = block index, both start at 1
+        self.log_prob = log_prob_init_value  # The sum log prob of this alignment over the target indices
         self.alignment_locations = []  # At which indices in the target output we need to insert <e>
         self.last_state_transducer = None  # Transducer state, shape [2, 1, transducer_hidden_units]
 
-    def insert_alignment(self, index, block_index, log_prob):
+    def __compute_sum_probabilities(self, transducer_outputs, targets, transducer_amount_outputs):
+        def get_prob_at_timestep(timestep):
+            return transducer_outputs[timestep][0][targets[start_index + timestep]]  # Debug
+            #return np.log(transducer_outputs[timestep][0][targets[start_index + timestep]])
+        start_index = self.alignment_position[0]-transducer_amount_outputs  # The current position this alignment is at
+        prob = log_prob_init_value
+        for i in range(0, transducer_amount_outputs):
+            prob += get_prob_at_timestep(i)
+        return prob
+
+    def insert_alignment(self, index, block_index, transducer_outputs, targets, transducer_amount_outputs,
+                         new_transducer_state):
+        """
+        Inserts alignment properties for a new block.
+        :param index: The index of of y~ corresponding to the last target index.
+        :param block_index: The new block index.
+        :param transducer_outputs: The computed transducer outputs.
+        :param targets: The complete target array, should be of shape [total_target_length].
+        :param transducer_amount_outputs: The amount of outputs that the transducer created in this block.
+        :param new_transducer_state: The new transducer state of shape [2, 1, transducer_hidden_units]
+        :return:
+        """
         self.alignment_locations.append(index)
         self.alignment_position = (index, block_index)
-        self.log_prob = log_prob
-
-    def insert_states(self, state_transducer):
-        self.last_state_transducer = state_transducer
-
+        # TODO: look if new log_prob is done additively or absolute
+        self.log_prob += self.__compute_sum_probabilities(transducer_outputs, targets, transducer_amount_outputs)
+        self.last_state_transducer = new_transducer_state
 
 # ----------------- Model -------------------------------
-
 embeddings = tf.Variable(tf.random_uniform([vocab_size, input_embedding_size], -1.0, 1.0), dtype=tf.float32)
 
 
@@ -142,32 +163,38 @@ encoder_raw_outputs, trans_hidden_state, transducer_amount_outputs, \
 # ----------------- Alignment --------------------------
 
 
-# TODO: manage these variables
-last_encoder_state = None
-last_transducer_state = None
-
-
-# TODO: block_index, targets etc. to start at index 1
-def run_new_block(session, block_inputs, previous_alignments, block_index, transducer_width, targets, total_blocks,
-                  targets_length):
-    # TODO: Get encoder outputs and save encoder state                                                      [p]
-    # TODO: Run transducer and create transducer_width new alignments for each existing alignments          [p]
-    # TODO: Note that the new alignments need to be maximum length of target                                [p, recalc]
-    # TODO: Manage new alignements
-    # TODO: Calculate for each alignment the sum log prob
-    # TODO: Filter each alignment with the same indices to contain the highest log prob
-
+def run_new_block(session, block_inputs, previous_alignments, block_index, transducer_max_width, targets, total_blocks,
+                  last_encoder_state):
+    """
+    Runs one block of the alignment process.
+    :param session: The current TF session.
+    :param block_inputs: The truncated inputs for the current block. Shape: [block_time, 1, input_dimensions]
+    :param previous_alignments: List of alignment objects from previous block step.
+    :param block_index: The index of the current new block.
+    :param transducer_width: The max width of the transducer block.
+    :param targets: The full target array of shape [time]
+    :param total_blocks: The total amount of blocks.
+    :param last_encoder_state: The encoder state of the previous step.
+    :return: new_alignments as list of Alignment objects,
+    last_encoder_state_new in shape of [2, 1, encoder_hidden_units]
+    """
+    # TODO: Get encoder outputs and save encoder state                                                      [p, d1]
+    # TODO: Run transducer and create transducer_width new alignments for each existing alignments          [p, d1]
+    # TODO: Note that the new alignments need to be maximum length of target                                [p, d]
+    # TODO: Manage new alignments                                                                           [p. d]
+    # TODO: Filter each alignment with the same indices to contain the highest log prob                     [p, d]
 
     def run_encoder(session, encoder_state, inputs):
         """
         Runs the encoder on specified inputs. Returns the outputs and encoder state.
         :param session: Current session.
         :param encoder_state: Current Encoder state, shape [2, 1, encoder_hidden_units]
-        :param inputs: The truncated inputs for this block.
+        :param inputs: The truncated inputs for this block. Shape [block_time, 1, input_dimensions]
         :return: Encoder ouputs [max_time, batch_size, encoder_hidden_units], Encoder state [2, 1, encoder_hidden_units]
         """
 
         # Inputs
+        """
         enc_out, enc_state = session.run([encoder_outputs, encoder_hidden_state_new],
                                          feed_dict={
                                              encoder_inputs: inputs,
@@ -175,7 +202,9 @@ def run_new_block(session, block_inputs, previous_alignments, block_index, trans
                                              encoder_hidden_state: encoder_state,
 
                                          })
-
+        """
+        enc_out = np.random.uniform(-1.0, 1.0, size=(input_block_size, 1, encoder_hidden_units))
+        enc_state = None
         return enc_out, enc_state
 
     def run_transducer(session, transducer_state, encoder_outputs, transducer_width):
@@ -185,45 +214,132 @@ def run_new_block(session, block_inputs, previous_alignments, block_index, trans
         :param transducer_state: The last transducer state as [2, 1, transducer_hidden_units] tensor.
         :param encoder_outputs: The outputs of the encoder on the input.
         :param transducer_width: The amount of outputs the transducer should produce.
-        :return: Transducer logits [transducer_amount_outputs, batch_size(1), vocab_size],
+        :return: Transducer logits [transducer_width, batch_size(1), vocab_size],
         Transducer state [2, 1, transducer_hidden_units]
         """
-
+        """
         trans_out, trans_state = session.run([logits, transducer_hidden_state_new],
                                              feed_dict={
                                                  encoder_raw_outputs: encoder_outputs,
                                                  trans_hidden_state: transducer_state,
                                                  transducer_amount_outputs: transducer_width
                                              })
+                                             """
+        trans_out = np.asarray([[[0.1, 0.7 + np.random.uniform(-0.15, 0.15), 0.2]]] * transducer_width)
+        trans_state = None
         return trans_out, trans_state
 
-    def compute_sum_probabilities(transducer_outputs, targets, alignment, transducer_amount_outputs):
-        def get_prob_at_timestep(timestep):
-            return np.log(transducer_outputs[timestep][1][targets[start_index + timestep]])
-
-        start_index = alignment.alignment_position[0]  # The current position this alignment is at
-        prob = 0
-        for i in range(0, transducer_amount_outputs):
-            prob += get_prob_at_timestep(i)
-
-        return prob
-
     # Run encoder and get inputs
-    block_encoder_outputs, last_encoder_state = run_encoder(session, last_encoder_state, block_inputs)
+    block_encoder_outputs, last_encoder_state_new = run_encoder(session, last_encoder_state, block_inputs)
 
     # Look into every existing alignment
+    new_alignments = []
     for i in range(len(previous_alignments)):
         alignment = previous_alignments[i]
 
-        # Expand the alignment for each transducer width
-        min_index = alignment.alignment_position[0] + \
-                    max(0, targets_length - (
-                            (total_blocks - block_index) * transducer_width) + transducer_width)  # TODO: calculate this
-        max_index = alignment.alignment_position[0] + transducer_width + min(0, targets_length - (
-                alignment.alignment_position[0] + transducer_width))
-        # for new_alignment in range(min_index, max_index+1):
-        # TODO: continue here
+        # Expand the alignment for each transducer width, only look at valid options
+        targets_length = len(targets)
+        min_index = alignment.alignment_position[0] + transducer_max_width + \
+                    max(-transducer_max_width, targets_length - ((total_blocks - block_index + 1) * transducer_max_width
+                                                             + alignment.alignment_position[0]))
+        max_index = alignment.alignment_position[0] + transducer_max_width + min(0, targets_length - (
+                alignment.alignment_position[0] + transducer_max_width))
 
+        # new_alignment_index's value is equal to the index of y~ for that computation
+        #print str(i) + ' ' + str(min_index) + '-' + str(max_index)
+        for new_alignment_index in range(min_index, max_index+1):  # +1 so that the max_index is also used
+            #print 'Alignment index: ' + str(new_alignment_index)
+            # Create new alignment
+            new_alignment = copy.deepcopy(alignment)
+            new_alignment_width = new_alignment_index - new_alignment.alignment_position[0]
+            trans_out, trans_state = run_transducer(session, alignment.last_state_transducer, block_encoder_outputs,
+                                                    new_alignment_width+1)  # +1 due to the last symbol being <e>
+            new_alignment.insert_alignment(new_alignment_index, block_index, trans_out, targets,
+                                           new_alignment_width, trans_state)
+            new_alignments.append(new_alignment)
+
+    # Delete all overlapping alignments, keeping the highest log prob
+    for a in reversed(new_alignments):
+        for o in new_alignments:
+            if o is not a and a.alignment_position == o.alignment_position and o.log_prob > a.log_prob:
+                if a in new_alignments:
+                    new_alignments.remove(a)
+
+    return new_alignments, last_encoder_state_new
+
+
+def get_alignment(session, inputs, targets, input_block_size, transducer_max_width):
+    # inputs of shape: [max_time, 1, input_dimensions]
+    # targets of shape: [time]
+
+    # TODO: Manage new blocks                       [p]
+    # TODO: Manage variables                        [p]
+    # TODO: block_index etc. to start at index 1    [p]
+
+    # Manage variables
+    amount_of_input_blocks = int(np.ceil(inputs.shape[0] / input_block_size))
+    current_block_index = 1
+    current_alignments = [Alignment()]
+    last_encoder_state = None  # TODO: check if this is right
+
+    # TODO: see if the +1 at the end is correct
+    for block in range(current_block_index, amount_of_input_blocks+1):
+        trunc_inputs = inputs[((block - 1) * input_block_size):block * input_block_size]
+
+        current_alignments, last_encoder_state = run_new_block(session, block_inputs=trunc_inputs,
+                                                               previous_alignments=current_alignments,
+                                                               block_index=block,
+                                                               transducer_max_width=transducer_max_width,
+                                                               targets=targets, total_blocks=amount_of_input_blocks,
+                                                               last_encoder_state=last_encoder_state)
+
+    # Check if we've found an alignment
+    assert len(current_alignments) == 1
+
+    return current_alignments[0].alignment_locations
+
+
+# ---------------------- Testing -----------------------------
+# TODO: Create testing environment for get_alignment
+
+
+testy_targets = np.asarray([1, 1, 1, 1, 1])
+
+
+def test_new_block():
+    def run_block(block_index, prev):
+        na, _ = run_new_block(None, block_inputs=None, previous_alignments=prev, block_index=block_index,
+                              transducer_max_width=3, targets=testy_targets, total_blocks=4, last_encoder_state=None)
+        return na
+    na = [Alignment()]
+    for i in range(0, 3):
+        na = run_block(i+2, na)
+        print 'Round ' + str(i+1) +' -----------'
+        for a in na:
+            print 'Alignment: ' + str(a.alignment_position)
+            print a.log_prob
+            print a.alignment_locations
+        print ''
+
+
+# Testing the alignment class
+def test_alignment_class():
+    testyAlignment = Alignment()
+    testy_outputs = np.asarray([[[0.1, 0.7, 0.2]], [[0.2, 0.1, 0.7]]])
+    testyAlignment.insert_alignment(2, 1, testy_outputs, testy_targets, 2, None)
+    print 'Log prob for test 1: ' + str(testyAlignment.log_prob)  # Should be: -2.65926003693
+    testy_outputs = np.asarray([[[0.2, 0.7, 0.1]], [[0.3, 0.1, 0.6]], [[0.2, 0.1, 0.7]]])
+    testyAlignment.insert_alignment(5, 2, testy_outputs, testy_targets, 3, None)
+    print 'Log prob for test 2: ' + str(testyAlignment.log_prob)  # Should be: -4.96184512993
+    print testyAlignment.alignment_locations
+
+
+def test_get_alignment():
+    testy_inputs = np.random.uniform(-1.0, 1.0, size=(10, 1, 5))
+    print get_alignment(None, inputs=testy_inputs, targets=testy_targets, input_block_size=2, transducer_max_width=2)
+
+
+test_get_alignment()
 
 # ---------------------- Management -----------------------------
 
