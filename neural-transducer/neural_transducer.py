10fb2b7e84a89fefcbf2e7a8e74a3789f8c88964
nikita68 <n.makarov@hotmail.com>
2018-03-13 12:17:15 +0100

    NT: debugging

--- neural-transducer/neural_transducer.py
+++ neural-transducer/neural_transducer.py
@@ -25,7 +25,8 @@ class ConstantsManager(object):
     def __init__(self, input_dimensions, input_embedding_size, inputs_embedded, encoder_hidden_units,
                  transducer_hidden_units, vocab_ids, input_block_size, beam_width, encoder_hidden_layers,
                  transducer_max_width, path_to_model, path_to_inputs, path_to_targets, path_to_alignments,
-                 path_to_cons_manager, amount_of_aligners, device_to_run, device_soft_placement):
+                 path_to_cons_manager, amount_of_aligners, device_to_run, device_soft_placement,
+                 debug_devices):
         assert transducer_hidden_units == 2 * encoder_hidden_units, 'Transducer has to have 2 times the amount ' \
                                                                     'of the encoder of units'
         # Vocab vars
@@ -58,6 +59,8 @@ class ConstantsManager(object):
         self.amount_of_aligners = amount_of_aligners
         self.device_to_run = device_to_run
         self.device_soft_placement = device_soft_placement
+        self.debug_devices = debug_devices
+
 
     # TODO: add lookup function for vocab
 
--- neural-transducer/nt_rimes.py
+++ neural-transducer/nt_rimes.py
@@ -30,7 +30,7 @@ def main():
                                          path_to_model=model_save, path_to_inputs=input_save, path_to_targets=target_save,
                                          path_to_alignments=alignments_save, path_to_cons_manager=cons_man_save,
                                          amount_of_aligners=int(sys.argv[2]), device_to_run=str(sys.argv[1]),
-                                         device_soft_placement=True)
+                                         device_soft_placement=True, debug_devices=bool(sys.argv[3]))
     with tf.device(constants_manager.device_to_run):  # Set device here
         model = Model(cons_manager=constants_manager)
 
